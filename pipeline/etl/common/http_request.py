a9e5ff6e625c9876cefb98b7c9c3f0c291f45948
mnaatjes <michael.naatjes87@gmail.com>
2026-02-01 20:33:30 +0000

    branching after testing pytest

--- pipeline/etl/common/http_request.py
+++ pipeline/etl/common/http_request.py
@@ -2,38 +2,24 @@
 import requests
 
 def http_request(url, method='GET', headers={}, timeout=30):
-
-    # GET HTTP Request
-    try:
         # Match method to Request
         match method.upper():
             # GET
             case 'GET':
-                # Get Response
-                res = requests.get(url, headers=headers, timeout=timeout)
+                return http_get(url, headers=headers, timeout=timeout)
 
             # Default Unsupported Case
             case _:
-                raise Exception("Unsupported HTTP Method!")
-    
-        # Check for response obj
-        if res:
-            # Raise for excptions
-            res.raise_for_status()
-            # Return Response
-            return res
+                raise ValueError(f"Unsupported HTTP Method {method}")
 
-    # Errors and Exceptions
-    except requests.exceptions.HTTPError as err_http:
-        return f"Error - HTTP: {err_http}"
-    except requests.exceptions.ConnectionError as err_conn:
-        print(f"Error - Connection: {err_conn}")
-    except requests.exceptions.Timeout as err_time:
-        print(f"Error - Timeout: {err_time}")
-    except requests.exceptions.RequestException as err_req:
-        print(f"Error - Unexpected Request: {err_req}")
-    # Return Default Value
-    return None
-
-def http_request_stream(url, method='GET', headers={}, stream=False, timeout=30):
-    return None
+"""
+Perform HTTP GET Request
+args: url (str): The URL to send the GET request to
+      headers (dict): Optional headers to include in the request
+      timeout (int): Timeout for the request in seconds
+returns: response (requests.Response): The response object from the GET request
+"""
+def http_get(url, headers={}, timeout=30):
+    response = requests.get(url, headers=headers, timeout=timeout)
+    response.raise_for_status()
+    return response
--- pipeline/etl/tests/conftest.py
+++ pipeline/etl/tests/conftest.py
@@ -1,16 +1,14 @@
 import pytest
 import os
 import json
+from utils import load_json
 
-# Helper - Load Destination Test Data from JSON file
-def load_json(filename):
-    fp = os.path.join(os.path.dirname(__file__), 'sample_data', filename)
-
-    with open(fp, 'r') as file:
-        data = json.load(file)
-    return data
-
-# Load Test URL Data
 @pytest.fixture(scope="session")
-def load_url_data():
-    return load_json("url_test_cases.json")
+def squares_data():
+    file_path = os.path.join(os.path.dirname(__file__), 'squares.json')
+    return load_json(file_path)
+
+@pytest.fixture(scope="session")
+def url_data():
+    file_path = os.path.join(os.path.dirname(__file__), 'sample_data/http_data.json')
+    return load_json(file_path)
