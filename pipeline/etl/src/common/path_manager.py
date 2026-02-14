27533295246292a6890c6bdac77b9d2851eca0d8
mnaatjes <michael.naatjes87@gmail.com>
2026-02-14 02:36:53 +0000

    refactoring

--- pipeline/etl/src/common/path_manager.py
+++ pipeline/etl/src/common/path_manager.py
@@ -1,8 +1,16 @@
 import re
+import json
 from pathlib import Path
 from datetime import datetime
+from typing import Type, TypeVar, Any
+from pydantic import BaseModel, ValidationError, TypeAdapter
+
+# --- Source Code ---
 from etl.src.config import orchestration
 from ..config.model import ETLConfig
+
+# --- TypeVar Declarations ---
+T = TypeVar("T", bound=BaseModel)
 
 class PathManager:
     def __init__(self, config: ETLConfig):
@@ -72,6 +80,64 @@ class PathManager:
     def create_directory(self, path: Path) -> None:
         """Creates a Directory - if it doesn't exist"""
         path.mkdir(parents=True, exist_ok=True)
-    
 
-    # --- Readers ---
+    # --- TODO: CSV I/O
+
+    # --- JSON I/O: Simple JSON Read/Write Methods ---
+
+    def read_json(self, path: Path) -> Any:
+        """Reads and parses a JSON file into raw py object (dict or list)"""
+        try:
+            with open(path, "r") as f:
+                return json.load(f)
+        except FileNotFoundError:
+            return None
+        except json.JSONDecodeError as e:
+            raise ValueError(f"Error decoding JSON from path {path}: {e}")
+    
+    def write_json(self, path:Path, data:Any, atomic:bool=True) -> None:
+        """
+        Writes a raw py object (dict or list) to a file
+        - Parent Dir MUST Exist
+        - If Atomic=True: uses tmp file to prevent corruption
+        """
+        if atomic:
+            tmp_path = path.with_suffix(f"{path.suffix}.tmp")
+            with open(tmp_path, "w") as f:
+                json.dump(data, f, indent=4, default=str)
+            tmp_path.replace(path)
+        else:
+            with open(path, "w") as f:
+                json.dump(data, f, indent=4, default=str)
+
+    # --- Pydantic Model I/O: Reads/Writes JSON Files with Pydantic Models
+
+    def read_pydantic_model(self, path:Path, model_class: Type[T]) -> T|None:
+        """
+        1) Reads a JSON file and validates it via Pydantic model.
+        2) Returns model instance or None
+        3) Raises ValueError on Validation or JSON decoding errors
+        """
+        # Use read_json() to get data from filepath
+        raw_data = self.read_json(path)
+        if raw_data is None:
+            return None # file empty
+        
+        # Perform pydantic model validation
+        try:
+            # Pydantic performs validation
+            # Create TypeAdapter to avoid parsing errors
+            adapter = TypeAdapter(model_class)
+            return adapter.validate_python(raw_data)
+            #return model_class.model_validate(raw_data)
+        except ValidationError as e:
+            raise ValueError(f"Pydantic validation failed for {path}: {e}")
+    
+    def write_pydantic_model(self, path:Path, model:BaseModel, atomic:bool=True) -> None:
+        """
+        Writes a Pydantic model to a JSON file
+        - Parent directory MUST exist
+        """
+        data = model.model_dump(mode="json")
+        self.write_json(path, data, atomic)
+
--- pipeline/etl/tests/test_path_mgr.py
+++ pipeline/etl/tests/test_path_mgr.py
@@ -1,9 +1,14 @@
 #import pytest
 from ..src.config.model import ETLConfig
 from ..src.common.path_manager import PathManager
+from ..src.common.manifests.manager import ManifestManager
 
 def test_path_manager():
     conf = ETLConfig()
     fm = PathManager(config=conf)
 
+    manifest_mgr = ManifestManager(fm.get_manifests_dir(), conf.version)
 
+    manifest = manifest_mgr.downloads
+    test_case = manifest.load()
+    print(test_case)
