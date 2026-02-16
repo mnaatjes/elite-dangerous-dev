4f42ec4b982323699b5072c3d46d38547fd4bc45
mnaatjes <michael.naatjes87@gmail.com>
2026-02-16 16:09:56 +0000

    banching to manifest

--- pipeline/tests/integration/test_run.py
+++ pipeline/tests/integration/test_run.py
@@ -19,28 +19,5 @@ def test_workflow():
 
     # --- Initialize Metadata Service ---
     meta = MetadataService(settings, pm)
-
-    meta.register_download(
-        content_sha256="e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
-        file_path=pm.generate_download_path(
-            source="downloads",
-            dataset="systems",
-            timestamp=datetime.now().strftime("%H%m%d_%H%I%S"),
-            version=settings.version,
-            service="extractor",
-            extension="json.gz"
-        ),
-        version="0.3",
-        pipeline="extractor",
-        service="download",
-        created_at=datetime.now().strftime("%Y%m%d_%H%I%S"),
-        source_url="http://spansh.co.uk",
-        source_name="edsm",
-        dataset="systems",
-        mime_type="",
-        compressed_size=1024,
-        compression_type=None,
-        is_valid=True
-    )
     
 
